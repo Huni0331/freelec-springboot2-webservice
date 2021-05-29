1480a778c09ae17cc42409a40a1b159a689add92
Hun <wjdqudgns4@nave.com>
2021-05-29 13:48:59 +0900

    8장 시작

--- src/main/java/com/huni/book/springboot/web/IndexController.java
+++ src/main/java/com/huni/book/springboot/web/IndexController.java
@@ -1,6 +1,9 @@
 package com.huni.book.springboot.web;
 
 
+import com.huni.book.springboot.config.auth.LoginUser;
+import com.huni.book.springboot.config.auth.dto.SessionUser;
+import com.huni.book.springboot.domain.user.User;
 import com.huni.book.springboot.service.posts.PostsService;
 import com.huni.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +12,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
+
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("googleName", user.getName());
+        }
+
         return "index";
     }
 
